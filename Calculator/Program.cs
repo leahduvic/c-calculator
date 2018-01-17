ed710ad4e14f4575af1ec824b1edde64877d1b85
leah duvic <leaheduvic@gmail.com>
2018-01-16 21:23:23 -0600

    finally

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,9 @@ namespace Calculator
         {
             Calculator calc = new Calculator();
             Console.WriteLine(calc.mathAddition(54, 29));
+            Console.WriteLine(calc.mathSubtraction(54, 29));
+            Console.WriteLine(calc.mathMultipy(54, 29));
+            Console.WriteLine(calc.mathDivide(54, 29));
         }
     }
 }
